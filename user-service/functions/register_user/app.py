01a3761967e411fc97fb627bd43abc412e1eb5c5
Maikel <mreijneke@student.che.nl>
2024-01-09 00:20:03 +0100

    registration implementation done with email fixed and improved

--- user-service/functions/register_user/app.py
+++ user-service/functions/register_user/app.py
@@ -1,49 +1,34 @@
 import json
 import os
 import boto3
+from botocore.exceptions import ClientError
 
 client = boto3.client('cognito-idp', region_name='us-east-1')
 client_id = os.environ['USER_POOL_CLIENT_ID']
+user_pool_id = os.environ['USER_POOL_ID']
 
-import json
-from os import environ
-import boto3
 
-event_bus = boto3.client('events')
-
-def validate_password(password):
-    errors = []
-
-    if len(password) < 8:
-        errors.append("Password must be at least 8 characters long.")
-
-    if not any(char.isupper() for char in password):
-        errors.append("Password must contain at least one uppercase letter.")
-
-    if not any(char.islower() for char in password):
-        errors.append("Password must contain at least one lowercase letter.")
-
-    if not any(char.isdigit() for char in password):
-        errors.append("Password must contain at least one digit.")
-
-    if not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>/?`~" for char in password):
-        errors.append("Password must contain at least one special character.")
-
-    return errors
+def is_email_already_registered(email):
+    try:
+        response = client.admin_get_user(
+            UserPoolId=user_pool_id,
+            Username=email
+        )
+        return True
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "UserNotFoundException":
+            return False
+        else:
+            return {
+                "status_code": 500,
+                "message": str(e)
+            }
 
 
 def lambda_handler(event, context):
     """
         RegisterUserFunction: Lambda function to validate and register a new user in the Cognito User Pool
     """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     print(f'event: {event}')
     print(f'eventtype: {type(event)}')
@@ -54,31 +39,47 @@ def lambda_handler(event, context):
     password = body['password']
     email = body['email']
 
-    validation_errors = validate_password(password)
+    email_exists = is_email_already_registered(email)
 
-    if validation_errors:
+    if email_exists is True:
         return {
-            "statusCode": 400,
+            "statusCode": 409,
             "body": json.dumps({
-                "message": "Password validation failed",
-                "errors": validation_errors
+                "message": "This email address is already in use"
+            })
+        }
+    elif email_exists is False:
+        response = client.sign_up(
+            ClientId=client_id,
+            Username=email,
+            Password=password,
+            UserAttributes=[
+                {'Name': 'preferred_username', 'Value': username}
+            ]
+        )
+
+        return {
+            "statusCode": 201,
+            "body": json.dumps({
+                "message": "User registered successfully",
+                "userSub": response['UserSub']
+            })
+        }
+    else:
+        return {
+            "statusCode": email_exists["status_code"],
+            "body": json.dumps({
+                "message": email_exists["message"]
             })
         }
 
-    response = client.sign_up(
-        ClientId=client_id,
-        Username=username,
-        Password=password,
-        UserAttributes=[
-            {'Name': 'email', 'Value': email}
-        ]
-    )
+    
 
-    return {
-        "statusCode": 201,
-        "body": json.dumps({
-            "message": "User registered successfully",
-            "userSub": response['UserSub']
-            # "location": ip.text.replace("\n", "")
-        })
-    }
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
