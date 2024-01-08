074f983e6cc2fa6a6b700aca654462597a387e97
Maikel <mreijneke@student.che.nl>
2024-01-08 17:58:28 +0100

    registration implementation done fixed

--- user-service/functions/register_user/app.py
+++ user-service/functions/register_user/app.py
@@ -5,27 +5,36 @@ import boto3
 client = boto3.client('cognito-idp', region_name='us-east-1')
 client_id = os.environ['USER_POOL_CLIENT_ID']
 
+import json
+from os import environ
+import boto3
+
+event_bus = boto3.client('events')
+
+def validate_password(password):
+    errors = []
+
+    if len(password) < 8:
+        errors.append("Password must be at least 8 characters long.")
+
+    if not any(char.isupper() for char in password):
+        errors.append("Password must contain at least one uppercase letter.")
+
+    if not any(char.islower() for char in password):
+        errors.append("Password must contain at least one lowercase letter.")
+
+    if not any(char.isdigit() for char in password):
+        errors.append("Password must contain at least one digit.")
+
+    if not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>/?`~" for char in password):
+        errors.append("Password must contain at least one special character.")
+
+    return errors
+
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+        RegisterUserFunction: Lambda function to validate and register a new user in the Cognito User Pool
     """
 
     # try:
@@ -36,9 +45,25 @@ def lambda_handler(event, context):
 
     #     raise e
 
-    username = event['username']
-    password = event['password']
-    email = event['email']
+    print(f'event: {event}')
+    print(f'eventtype: {type(event)}')
+
+    body = json.loads(event['body'])
+
+    username = body['username']
+    password = body['password']
+    email = body['email']
+
+    validation_errors = validate_password(password)
+
+    if validation_errors:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Password validation failed",
+                "errors": validation_errors
+            })
+        }
 
     response = client.sign_up(
         ClientId=client_id,
