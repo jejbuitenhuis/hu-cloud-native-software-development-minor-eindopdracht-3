ba521da6dd15d54d659cc79eb04c48e2b7085fa3
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2024-01-08 06:54:08 -0800

    Update app.py
    Added x-ray en logging

--- user-service/functions/Ping/app.py
+++ user-service/functions/Ping/app.py
@@ -1,9 +1,15 @@
 import json
 from os import environ
 import boto3
+from aws_xray_sdk.core import patch_all
+import logging
+
+if 'DISABLE_XRAY' not in os.environ:
+    patch_all()
 
 event_bus = boto3.client('events')
-
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 def lambda_handler(event, context):
     event = {
@@ -15,6 +21,8 @@ def lambda_handler(event, context):
         'EventBusName': environ['EVENT_BUS_ARN']
     }
     event_bus.put_events(Entries=[event])
+
+    logger.info("hello world!")
 
     return {
         "statusCode": 200,
