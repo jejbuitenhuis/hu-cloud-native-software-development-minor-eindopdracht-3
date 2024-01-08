d88af6eb62b6b1d8310a24322a0cb031619a3798
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-01-08 16:40:55 +0100

    Fixed x-ray en added some requirements

--- user-service/functions/Ping/app.py
+++ user-service/functions/Ping/app.py
@@ -4,12 +4,13 @@ import boto3
 from aws_xray_sdk.core import patch_all
 import logging
 
-if 'DISABLE_XRAY' not in os.environ:
+if 'DISABLE_XRAY' not in environ:
     patch_all()
 
 event_bus = boto3.client('events')
 logger = logging.getLogger()
 logger.setLevel("INFO")
+
 
 def lambda_handler(event, context):
     event = {
