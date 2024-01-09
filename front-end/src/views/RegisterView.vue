f6af66e7a2b9ae31b779d3e400e12a13571acb60
amohacker <tnvtablet@gmail.com>
2024-01-09 17:17:30 +0100

    Added connection to back-end. Added a go to login button.
    Co-Authored-By: Ruben van der Hout <71267785+RubenvanderHout@users.noreply.github.com>

--- front-end/src/views/RegisterView.vue
+++ front-end/src/views/RegisterView.vue
@@ -2,36 +2,61 @@
 <sl-card class="card-header">
 <h1>Register</h1>
 <form id="login-form" @submit.prevent.submit="register">
-    <sl-input label="Email" id="email" type="email" placeholder="Email" required v-model="username" ref="usernameInput"></sl-input>
+    <sl-input label="Email" id="email" type="email" placeholder="Email" required v-model="email" ref="emailInput"></sl-input>
     <sl-input label="Password" id="password" password-toggle placeholder="Password" type="password" required v-model="password" @sl-input="handleInput" ref="passwordInput"></sl-input>
     <sl-input label="Re-enter password" id="confirm" password-toggle placeholder="Confirm password" type="password" required v-model="confirmPassword" @sl-input="handleInput" ref="passwordConfirmInput"></sl-input>
-    <br><p id="error-message" v-if="errorMessage">{{ errorMessage }}</p>
-    <sl-button class="confirm" type="submit">Confirm</sl-button>
+    <p id="error-message" v-if="errorMessage">{{ errorMessage }}</p>
+    <sl-button class="confirm" variant="primary" type="submit">Confirm</sl-button>
+    <sl-button class="gologin" variant="neutral" @click="router.push('/login')">To login</sl-button>
 </form>
 </sl-card>
 </template>
+
+
+<!-- SCRIPT -->
+
 
 <script setup lang="ts">
 import "@shoelace-style/shoelace/dist/components/input/input";
 import '@shoelace-style/shoelace/dist/components/card/card';
 import type SlInput from "@shoelace-style/shoelace/dist/components/input/input";
-import { onMounted, ref, type Ref } from "vue";
+import { ref, type Ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
-const username = ref('');
+const email = ref('');
 const password = ref('');
 const confirmPassword = ref('');
 
-const usernameInput : Ref<null> | Ref<HTMLObjectElement> = ref(null);
+const emailInput : Ref<null> | Ref<HTMLObjectElement> = ref(null);
 const passwordInput : Ref<null> | Ref<HTMLObjectElement> = ref(null);
 const passwordConfirmInput : Ref<null> | Ref<HTMLObjectElement> = ref(null);
 
 const errorMessage = ref('');
+
+const router = useRouter();
 
 function register(){
     if (!isFormValid()) {
         return
     }
     console.log("submitted");
+    sendData(email.value, password.value)
+        .then((response) => {
+            if (response.status === 409){
+                showErrorMessage("This email adress has already been registered!");
+            }
+            if (response.ok) {
+                alert("We have send you an email to verify your email adress.");
+                router.push("/login");
+            } else {
+                showErrorMessage(`Something went wrong: \n${response.statusText}`);
+            }
+        });
+}
+
+function showErrorMessage(error : string){
+    console.log(error);
+    errorMessage.value = error;
 }
 
 function handleInput(){
@@ -53,7 +78,7 @@ function handleInput(){
 
 function isFormValid() : boolean{
     // @ts-ignore typescript detecteerd het bestaan van getAttribute() niet
-    if (usernameInput.value.getAttribute('data-invalid') === ''){
+    if (emailInput.value.getAttribute('data-invalid') === ''){
         return false;
     }
     // @ts-ignore typescript detecteerd het bestaan van getAttribute() niet
@@ -68,11 +93,32 @@ function isFormValid() : boolean{
     return true;
 }
 
+async function sendData(email : string, password : string){
+    return await fetch("/api/auth/register", {
+        "method" : "post",
+        "mode" : "cors",
+        "headers" : {
+            "Content-Type" : "application/json"
+        },
+        "body" : JSON.stringify({
+            "email" : email,
+            "password" : password
+        })
+    }
+    )
+}
 </script>
+
+
+<!-- STYLE -->
 
 
 <style scoped lang="scss">
 .confirm {
     width: 100%;
+}
+.gologin {
+    width: 100%;
+    margin-top: 5px;
 }
 </style>
