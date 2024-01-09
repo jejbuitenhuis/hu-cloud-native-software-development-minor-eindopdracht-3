a24ecc0b376e9bc263fa0c488f7748b456be9ecc
Maikel <mreijneke@student.che.nl>
2024-01-09 22:25:47 +0100

    Integration test registration

--- user-service/functions/register_user/app.py
+++ user-service/functions/register_user/app.py
@@ -2,6 +2,13 @@ import json
 import os
 import boto3
 from botocore.exceptions import ClientError
+from aws_xray_sdk.core import patch_all
+import logging
+
+patch_all()
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 client = boto3.client('cognito-idp', region_name='us-east-1')
 client_id = os.environ['USER_POOL_CLIENT_ID']
@@ -9,6 +16,7 @@ user_pool_id = os.environ['USER_POOL_ID']
 
 
 def is_email_already_registered(email):
+    logger.info(user_pool_id)
     try:
         response = client.admin_get_user(
             UserPoolId=user_pool_id,
@@ -19,6 +27,7 @@ def is_email_already_registered(email):
         if e.response["Error"]["Code"] == "UserNotFoundException":
             return False
         else:
+            logger.error(str(e))
             return {
                 "status_code": 500,
                 "message": str(e)
@@ -30,8 +39,9 @@ def lambda_handler(event, context):
         RegisterUserFunction: Lambda function to validate and register a new user in the Cognito User Pool
     """
 
-    print(f'event: {event}')
-    print(f'eventtype: {type(event)}')
+    logger.info(f'EventType: {type(event)}')
+    logger.info(f'Event: {event}')
+    logger.info(user_pool_id)
 
     body = json.loads(event['body'])
 
@@ -40,15 +50,18 @@ def lambda_handler(event, context):
     email = body['email']
 
     email_exists = is_email_already_registered(email)
+    email_used_str = 'Email address is already in use.'
 
     if email_exists is True:
+        logger.info(email_used_str)
         return {
             "statusCode": 409,
             "body": json.dumps({
-                "message": "This email address is already in use"
+                "error": email_used_str
             })
         }
     elif email_exists is False:
+        logger.info("Email address is not registered. Proceeding with user registration.")
         response = client.sign_up(
             ClientId=client_id,
             Username=email,
@@ -58,22 +71,19 @@ def lambda_handler(event, context):
             ]
         )
 
+        logger.info(f'User Registered: {response}')
+
         return {
             "statusCode": 201,
-            "body": json.dumps({
-                "message": "User registered successfully",
-                "userSub": response['UserSub']
-            })
+            "body": json.dumps(response)
         }
     else:
         return {
             "statusCode": email_exists["status_code"],
             "body": json.dumps({
-                "message": email_exists["message"]
+                "error": email_exists["message"]
             })
         }
-
-    
 
 
     # try:
