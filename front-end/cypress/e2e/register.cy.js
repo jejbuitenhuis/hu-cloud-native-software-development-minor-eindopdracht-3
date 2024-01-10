3ff7c1b647e1cd4022ef4ad8722dc9c789dc0c41
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-01-10 16:06:08 +0100

    last changes

--- front-end/cypress/e2e/register.cy.js
+++ front-end/cypress/e2e/register.cy.js
@@ -8,7 +8,7 @@ describe("Register page Test", () => {
         cy.get('#email').find("input").type('test@example.com')
         cy.get('#password').find("input").type('testtest')
         cy.get('#confirm').find("input").type('testtest')
-        cy.get('#submit').find('button').click({ position: 'top' })
+        cy.get('#submit').find('button').clickAtTop()
         cy.contains("We have send you an email to verify your email adress.")
     })
 
@@ -16,13 +16,13 @@ describe("Register page Test", () => {
         cy.get('#email').find("input").type('test@example.com')
         cy.get('#password').find("input").type('testtest')
         cy.get('#confirm').find("input").type('testtest')
-        cy.get('#submit').find('button').click({ position: 'top' })
+        cy.get('#submit').find('button').clickAtTop()
         
         
         cy.get('#email').type('test@example.com')
         cy.get('#password').type('testtest')
         cy.get('#confirm').find('input').type('testtest')
-        cy.get('#submit').find('button').click({ position: 'top' })
+        cy.get('#submit').find('button').clickAtTop()
 
         cy.contains("This email adress has already been registered!")
     })
