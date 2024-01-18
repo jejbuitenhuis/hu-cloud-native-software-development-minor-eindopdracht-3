e01b90c550272fa65e579fe24ca851b5e0d6a6ac
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-01-18 15:15:39 +0100

    Added navigation to header

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -1,23 +1,34 @@
 <template>
-  <div id="title-wrapper">
+  <header id="title-wrapper">
     <h1>Dragons MTG Card Collection System</h1>
     <img
         src="/mtg_logo.png"
         alt="MTG Logo"
     >
-  </div>
+    <nav>
+      <router-link to="/collection">Collection</router-link>
+      <router-link to="/decks">Decks</router-link>
+      <router-link to="/search">Search</router-link>
+    </nav>
+  </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 #title-wrapper {
   display: flex;
   margin: 1rem 2rem;
   align-items: center;
-}
-h1 {
-  margin: 0;
-}
-#title-wrapper img {
-  height: 3em;
+
+  h1 {
+    margin: 0;
+  }
+  img {
+    height: 3em;
+  }
+  nav {
+    margin-left: auto;
+    display: flex;
+    gap: 1rem;
+  }
 }
 </style>
