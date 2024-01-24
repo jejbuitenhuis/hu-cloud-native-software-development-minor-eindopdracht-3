9494e8c23c048f6103df5fc3c41d177981e4a534
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-01-24 15:18:46 +0100

    add oracleId GSI

--- collection-service/functions/add_card/app.py
+++ collection-service/functions/add_card/app.py
@@ -9,7 +9,7 @@ import requests
 import uuid
 from os import environ
 
-if 'DISABLE_XRAY' not in environ:
+if "DISABLE_XRAY" not in environ:
     patch_all()
 
 LOGGER = logging.getLogger()
@@ -19,14 +19,14 @@ DYNAMO_DB = boto3.resource("dynamodb", region_name="us-east-1")
 DYNAMO_DB_COLLECTION_TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME")
 COLLECTION_TABLE = DYNAMO_DB.Table(DYNAMO_DB_COLLECTION_TABLE_NAME)
 STAGE = os.getenv("STAGE")
-ssm = boto3.client('ssm')
+ssm = boto3.client("ssm")
 
 
 def fetch_api_url():
     LOGGER.info("Fetching api url from ssm")
     try:
-        parameter = ssm.get_parameter(Name=f'/{STAGE}/MTGCardApi/url')
-        return parameter['Parameter']['Value']
+        parameter = ssm.get_parameter(Name=f"/{STAGE}/MTGCardApi/url")
+        return parameter["Parameter"]["Value"]
     except ClientError as e:
         LOGGER.error(f"Error while fetching api url from ssm: {e}")
         raise e
@@ -36,33 +36,38 @@ def parse_card_item(item, user_id, condition):
     card_instance_id = str(uuid.uuid4())
     face_items = []
 
-    for face in item['CardFaces']:
-        face_items.append({
-            "OracleText": face['OracleText'],
-            "ManaCost": face['ManaCost'],
-            "TypeLine": face['TypeLine'],
-            "FaceName": face['FaceName'],
-            "FlavorText": face['FlavorText'],
-            "ImageUrl": face['ImageUrl'],
-            "Colors": face['Colors'],
-            "LowercaseFaceName": face['LowercaseFaceName'],
-            "LowercaseOracleText": face['LowercaseOracleText']
-        })
+    for face in item["CardFaces"]:
+        face_items.append(
+            {
+                "OracleText": face["OracleText"],
+                "ManaCost": face["ManaCost"],
+                "TypeLine": face["TypeLine"],
+                "FaceName": face["FaceName"],
+                "FlavorText": face["FlavorText"],
+                "ImageUrl": face["ImageUrl"],
+                "Colors": face["Colors"],
+                "LowercaseFaceName": face["LowercaseFaceName"],
+                "LowercaseOracleText": face["LowercaseOracleText"],
+            }
+        )
 
     return {
-        "PK": f'UserId#{user_id}',
-        "SK": f'CardInstanceId#{card_instance_id}',
+        "PK": f"UserId#{user_id}",
+        "SK": f"CardInstanceId#{card_instance_id}",
         "PrintId": item["PrintId"],
-        "OracleId": item['OracleId'],
+        "OracleId": item["OracleId"],
         "CardInstanceId": card_instance_id,
         "Condition": condition,
-        "OracleName": item['OracleName'],
-        "SetName": item['SetName'],
-        "ReleasedAt": item['ReleasedAt'],
-        "Rarity": item['Rarity'],
-        "Price": item['Price'],
-        "LowerCaseOracleName": item['LowerCaseOracleName'],
+        "OracleName": item["OracleName"],
+        "SetName": item["SetName"],
+        "ReleasedAt": item["ReleasedAt"],
+        "Rarity": item["Rarity"],
+        "Price": item["Price"],
+        "LowerCaseOracleName": item["LowerCaseOracleName"],
         "CardFaces": face_items,
+        "DeckId": item["DeckId"],
+        "GSI1SK": f"DeckId#{item['DeckId']}#CardInstanceId#{card_instance_id}",
+        "GSI2SK": f"OracleId#{item['OracleId']}#CardInstanceId#{card_instance_id}",
     }
 
 
@@ -85,42 +90,39 @@ def lambda_handler(event, context):
     LOGGER.info("Starting add card to collection lambda")
 
     body = json.loads(event["body"])
-    oracle_id = body['oracle_id']
-    print_id = body['print_id']
-    condition = body['condition']
+    oracle_id = body["oracle_id"]
+    print_id = body["print_id"]
+    condition = body["condition"]
     user_id = get_user_id(event)
 
     try:
         api_url = fetch_api_url()
         api_response = requests.get(
-            f'{api_url}/api/cards/{oracle_id}/{print_id}',
-            headers={'Authorization': event["headers"]["Authorization"]}
+            f"{api_url}/api/cards/{oracle_id}/{print_id}",
+            headers={"Authorization": event["headers"]["Authorization"]},
         )
         api_response_code = api_response.status_code
         api_response_body = api_response.json()
 
-
         if api_response_code != 200:
             LOGGER.info(f"{api_response_code = }")
             LOGGER.info(f"{api_response_body = }")
-            api_error_message = api_response_body['Message']
-            LOGGER.error(f"Error while fetching card from api: {api_response_code}\n {api_error_message}")
+            api_error_message = api_response_body["Message"]
+            LOGGER.error(
+                f"Error while fetching card from api: {api_response_code}\n {api_error_message}"
+            )
             return {
                 "statusCode": api_response_code,
-                "body": json.dumps({"Message": api_error_message})
+                "body": json.dumps({"Message": api_error_message}),
             }
 
         saved_card = save_card_to_db(api_response_body, user_id, condition)
 
-        LOGGER.info(f"Successfully added the following card to the collection: {saved_card}")
+        LOGGER.info(
+            f"Successfully added the following card to the collection: {saved_card}"
+        )
 
-        return {
-            "statusCode": 201,
-            "body": json.dumps(saved_card)
-        }
+        return {"statusCode": 201, "body": json.dumps(saved_card)}
     except ClientError as e:
         LOGGER.error(f"Error while saving the card: {e}")
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"Message": e.response})
-        }
+        return {"statusCode": 500, "body": json.dumps({"Message": e.response})}
