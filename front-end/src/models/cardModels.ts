4d72d7b965718ebf496fd2dcf2f4e1fa6f60b843
amohacker <tnvtablet@gmail.com>
2024-01-24 16:34:05 +0100

    added a type for the mess that is a deck card

--- front-end/src/models/cardModels.ts
+++ front-end/src/models/cardModels.ts
@@ -19,8 +19,52 @@ export type PrintFace = {
   OracleText: string,
   TypeLine: string,
 }
-export interface CardData {
-  cardInstanceId?: string,
-  cardLocation: string,
-  card: PrintCard,
+export type DeckCard = {
+  PK: string,
+  SK: string,
+  card_location: string,
+  CardFaces: CardFaceWrapper[]
+  data_type: string,
+  deck_card_id: string,
+  deck_id: string,
+  OracleId: string,
+  OracleName: string,
+  Price: number,
+  PrintId: string,
+  Rarity: string,
+  ReleasedAt: string,
+  SetName: string,
+  user_id: string
+}
+
+export type CardFaceWrapper = {
+  M: CardFace
+}
+
+export type CardFace = {
+  Colors: {
+    L: Color[]
+  },
+  FlavorText: {
+    "S": string
+  },
+  ManaCost: {
+    "S": string
+  },
+  TypeLine: {
+    "S": string
+  },
+  ImageUrl?: {
+    "S": string
+  },
+  FaceName: {
+    "S": string
+  },
+  OracleText: {
+    "S": string
+  }
+}
+
+export type Color = {
+  "S": string
 }
